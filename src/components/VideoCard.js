0e7b9db5328b1977d17057fd66a39bfbbb5ce9e1
Olvine23 <olivinegeorge@gmal.com>
2021-11-24 15:24:35 -0800

    removed the bottom nav

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 const VideoCard = ( {video}) => {
     return (
         <div>
-            <h2>{video.user}</h2>
-            <img src ={video.userImageURL}></img>
+             
+             
             {video.id}
              
         </div>
